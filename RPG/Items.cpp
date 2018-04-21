f572524513c9f264da9927fe571233c93b6eeaae
KamilBlanik <235629@student.pwr.edu.pl>
2018-04-21 17:45:35 +0200

    .

--- RPG/Items.cpp
+++ RPG/Items.cpp
@@ -42,3 +42,67 @@ Items::~Items() {
 	bonusMana = 0;
 	itemLevel = 0;
 }
+
+void Items::setName(std::string name) {
+	this->name = name;
+}
+
+void Items::setType(std::string type) {
+	this->type = type;
+}
+
+void Items::setPrice(int price) {
+	this->price = price;
+}
+
+void Items::setDmg(int dmg) {
+	this->bonusDmg = dmg;
+}
+
+void Items::setHp(int hp) {
+	this->bonusHp = hp;
+}
+
+void Items::setMana(int mana) {
+	this->bonusMana = mana;
+}
+
+void Items::setArmor(int arm) {
+	this->bonusArmor = arm;
+}
+
+void Items::setItemLvl(int lvl) {
+	this->itemLevel = lvl;
+}
+
+std::string Items::getName() {
+	return name;
+}
+
+std::string Items::getType() {
+	return type;
+}
+
+int Items::getPrice() {
+	return price;
+}
+
+int Items::getDmg() {
+	return bonusDmg;
+}
+
+int Items::getHp() {
+	return bonusHp;
+}
+
+int Items::getMana() {
+	return bonusMana;
+}
+
+int Items::getArmor() {
+	return bonusArmor;
+}
+
+int Items::getItemLvl() {
+	return itemLevel;
+}
--- RPG/Items.h
+++ RPG/Items.h
@@ -26,7 +26,23 @@ public:
 		int itemLevel);
 	Items();
 	~Items();
+	void setName(std::string name);
+	void setType(std::string type);
+	void setPrice(int price);
+	void setDmg(int dmg);
+	void setHp(int hp);
+	void setMana(int mana);
+	void setArmor(int armor);
+	void setItemLvl(int lvl);
 
+	std::string getName();
+	std::string getType();
+	int getPrice();
+	int getDmg();
+	int getHp();
+	int getMana();
+	int getArmor();
+	int getItemLvl();
 };
 
 #endif
